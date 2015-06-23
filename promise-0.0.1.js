f0b751d3c1b9bce59d9a918b493032dd22943658
Cathy <weiyi_wang@yeah.net>
2015-06-23 15:17:42 +0800

    私有数据改为this._的形式实现

--- promise-0.0.1.js
+++ promise-0.0.1.js
@@ -6,17 +6,11 @@
 
 (function() {
 	'use strict';
-
 	// TODO: 这里可以设置一个开关，若传了则使用原生的，但这必须要作为模块调用
 	// TODO: 将Promise设计成可AMD 可CMD 可非模块引的形式
 	
 	//因FF及Chrome都没有处理相互解决的死锁情况，故舍弃浏览器的原生实现
 	// if(window.Promise && typeof window.Promise.then === 'function') return;
-
-
-	//私有数据缓存
-	var guid = 0,
-		cache = {};
 
 	/**
 	 * 构造方法
@@ -27,10 +21,14 @@
 	function Promise(task, internal) {
 		if(internal !== true && !isFunc(task)) {
 			throw new TypeError('task must be a function');
-		}
+		}  
 
-		this._guid = guid++;
-		setData(this._guid);
+		this._state = 'pending';
+		this._value = undefined;
+		this._reason = undefined;
+		this._onFulfilledArr = [];
+		this._onRejectedArr = [];
+		this._next = []; //promise chain
 
 		if(isFunc(task)) {
 			var promise = this, 
@@ -74,7 +72,7 @@
 			valueCount = 0,
 			valueArr = [],
 			addCallback = function(i) {
-				cache[promiseArr[i]._guid].onFulfilledArr.push(function(value) {
+				promiseArr[i]._onFulfilledArr.push(function(value) {
 					valueCount++;
 					valueArr[i] = value;
 
@@ -86,7 +84,7 @@
 					//即：当原onFulfilledArr为空而原promise被解决时，原promise的nextPromise能正确地以value被解决
 					return value; 
 				});
-				cache[promiseArr[i]._guid].onRejectedArr.push(function(reason) {
+				promiseArr[i]._onRejectedArr.push(function(reason) {
 					reject(promise, reason);
 
 					//保证原onRejected链的行为不被破坏.
@@ -103,7 +101,8 @@
 	};
 
 	/**
-	 * 	接受一个Promise数组作为参数， 返回一个新的promise：<br/>
+	 * 	接受一个Promise数组作为参数， 返
+	 * 	回一个新的promise：<br/>
 	 * 		当Promise数组中任一promise被解决后，该promise以相同的结果被解决<br/>
 	 *   	当Promise数组中任一promise被拒绝时，该promise以相同的原因被拒绝
 	 *
@@ -127,8 +126,8 @@
 			};		
 		
 		for(var i = 0, len = promiseArr.length; i < len; i++) {
-			cache[promiseArr[i]._guid].onFulfilledArr.push(onFulfilledCb);
-			cache[promiseArr[i]._guid].onRejectedArr.push(onRejectedCb);
+			promiseArr[i]._onFulfilledArr.push(onFulfilledCb);
+			promiseArr[i]._onRejectedArr.push(onRejectedCb);
 		}
 
 		return promise;
@@ -174,21 +173,22 @@
 				isonRejectedFunc = isFunc(onRejected);
 
 			if(isOnFulfilledFunc) {
-				cache[this._guid].onFulfilledArr.push(onFulfilled);
+				this._onFulfilledArr.push(onFulfilled);
 			}
 			
 			if(isonRejectedFunc) {
-				cache[this._guid].onRejectedArr.push(onRejected);
+				this._onRejectedArr.push(onRejected);
 			}
 
-			if(cache[this._guid].state === 'fulfilled') {
-				isOnFulfilledFunc && onFulfilled(cache[this._guid].value);
+			if(this._state === 'fulfilled') {
+				isOnFulfilledFunc && onFulfilled(this._value);
 
-			} else if(cache[this._guid].state === 'rejected') {
-				isonRejectedFunc && onRejected(cache[this._guid].reason);
+			} else if(this._state === 'rejected') {
+				isonRejectedFunc && onRejected(this._reason);
 			}
 
-			return getNext(this._guid);
+			this._next = new Promise(null, true);
+			return this._next;
 		},
 
 		/**
@@ -199,29 +199,32 @@
 		 * @return {Promise}  一个新的Promise对象
 		 */
 		catch: function(onRejected) {
-			if(isFunc(onRejected)) {
-				cache[this._guid].onRejectedArr.push(onRejected);
+			var isonRejectedFunc = isFunc(onRejected);
+			
+			if(isonRejectedFunc) {
+				this._onRejectedArr.push(onRejected);
 			}
 
-			if(cache[this._guid].state === 'rejected') {
-				onRejected(cache[this._guid].reason);
+			if(this._state === 'rejected') {
+				isonRejectedFunc && onRejected(this._reason);
 			}
 
-			return getNext(this._guid);
+			this._next = new Promise(null, true);
+			return this._next;
 		},
 
 		isFulfilled: function() {
-			return cache[this._guid].state === 'fulfilled';
+			return this._state === 'fulfilled';
 		},
 
 		isRejected: function() {
-			return cache[this._guid].state === 'rejected';
+			return this._state === 'rejected';
 		}
 	};
 
 	//解决：实现或拒绝
 	function resolve(promise, x) {
-		if(cache[promise._guid].state !== 'pending') {
+		if(promise._state !== 'pending') {
 			return;
 		}
 
@@ -235,20 +238,20 @@
 				return;
 			}
 
-			switch(cache[x._guid].state) {
+			switch(x._state) {
 				case 'pending':
 					//remain pending until x is fulfilled or rejected
-					cache[x._guid].onFulfilledArr = cache[x._guid].onFulfilledArr.concat(cache[promise._guid].onFulfilledArr);
-					cache[x._guid].onRejectedArr = cache[x._guid].onRejectedArr.concat(cache[promise._guid].onRejectedArr);
-					cache[x._guid].next = cache[promise._guid].next;
+					x._onFulfilledArr = x._onFulfilledArr.concat(promise._onFulfilledArr);
+					x._onRejectedArr = x._onRejectedArr.concat(promise._onRejectedArr);
+					x._next = promise._next;
 					break;
 
 				case 'fulfilled': 
-					fulfill(promise, cache[x._guid].value);
+					fulfill(promise, x._value);
 					break;
 
 				case 'rejected':
-					reject(promise, cache[x._guid].reason);
+					reject(promise, x._reason);
 					break;
 			}
 
@@ -303,23 +306,23 @@
 
 	//实现
 	function fulfill(promise, value) {
-		if(cache[promise._guid].state !== 'pending') {
+		if(promise._state !== 'pending') {
 			return;
 		}
 		
-		cache[promise._guid].state = 'fulfilled';
-		cache[promise._guid].value = value;
+		promise._state = 'fulfilled';
+		promise._value = value;
 
-		var f, rtv, nextPromise = cache[promise._guid].next;			
+		var f, rtv, nextPromise = promise._next;			
 		try {
-			if(cache[promise._guid].onFulfilledArr.length) {
-				for(var i = 0, len = cache[promise._guid].onFulfilledArr.length; i < len; i++) {
-					f = cache[promise._guid].onFulfilledArr[i]; //直接调用时this会指向onFulfilledArr这个数组
-					rtv = f(cache[promise._guid].value); //以函数方式调用，即非严格模式下promise为window, 严格模式下为undefined
+			if(promise._onFulfilledArr.length) {
+				for(var i = 0, len = promise._onFulfilledArr.length; i < len; i++) {
+					f = promise._onFulfilledArr[i]; //直接调用时this会指向onFulfilledArr这个数组
+					rtv = f(promise._value); //以函数方式调用，即非严格模式下promise为window, 严格模式下为undefined
 				}
 
 			} else {
-				nextPromise && fulfill(nextPromise, cache[promise._guid].value);
+				nextPromise && fulfill(nextPromise, promise._value);
 			}
 
 		} catch(e) {
@@ -331,23 +334,23 @@
 
 	//拒绝
 	function reject(promise, reason) {
-		if(cache[promise._guid].state !== 'pending') {
+		if(promise._state !== 'pending') {
 			return;
 		}
 
-		cache[promise._guid].state = 'rejected';
-		cache[promise._guid].reason = reason;
+		promise._state = 'rejected';
+		promise._reason = reason;
 
-		var f, rtv, nextPromise = cache[promise._guid].next;			
+		var f, rtv, nextPromise = promise._next;			
 		try {
-			if(cache[promise._guid].onRejectedArr.length) {
-				for(var i = 0, len = cache[promise._guid].onRejectedArr.length; i < len; i++) {
-					f = cache[promise._guid].onRejectedArr[i]; //直接调用时this会指向onRejectedArr这个数组
-					rtv = f(cache[promise._guid].reason); //以函数方式调用，即非严格模式下promise为window, 严格模式下为undefined
+			if(promise._onRejectedArr.length) {
+				for(var i = 0, len = promise._onRejectedArr.length; i < len; i++) {
+					f = promise._onRejectedArr[i]; //直接调用时this会指向onRejectedArr这个数组
+					rtv = f(promise._reason); //以函数方式调用，即非严格模式下promise为window, 严格模式下为undefined
 				}
 
 			} else {
-				nextPromise && reject(nextPromise, cache[promise._guid].reason);
+				nextPromise && reject(nextPromise, promise._reason);
 			}
 
 		} catch(e) {
@@ -360,9 +363,9 @@
 	//判断x是否在promise的解决队列中（即如果用x来解决promise会引起死循环）
 	function inQueue(promise, x) {
 		if(!promise.chain) {
-			//cache[promise._guid].next cannot be resolved from outside.
+			//promise._next cannot be resolved from outside.
 			//promise in array is to prevent promise --> x --> promise
-			promise.chain = x.chain = [cache[promise._guid].next, promise, x]; 
+			promise.chain = x.chain = [promise._next, promise, x]; 
 		
 		} else {
 			for(var i = 0, len = promise.chain.length; i < len; i++) {
@@ -373,29 +376,6 @@
 			promise.chain[len] = x;
 		}
 		return false;
-	}
-
-	function getNext(guid) {
-		if(!cache[guid].next) {
-			cache[guid].next = new Promise(null, true);
-
-		} else { 
-			//若存在则只重置数据，避免每次调用then()都新创建一个promise
-			//因每个promise的guid是唯一的，会在cache中注册新的数据对象
-			//若多次在同一个promise上调用then, 则最后一次之前调用then所生成的promise都会变成垃圾，
-			//其在cache上注册的数据虽然可以删除，但会造成cache上guid的不连续，个人不喜欢。
-			setData(cache[guid].next.guid);
-		}
-		return cache[guid].next;
-	}
-
-	//注册或重置私有数据
-	function setData(guid) {
-		cache[guid] = {
-			state: 'pending',
-			onFulfilledArr: [],
-			onRejectedArr: []
-		};
 	}
 
 	//判断一个值是否为函数
